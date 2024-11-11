09b239496d6b0829d3cbe2ec3bd1c45c73c86128
denq <denquick@gmail.com>
2024-11-11 21:00:58 +0300

    Errors

--- src/errors.ts
+++ src/errors.ts
@@ -7,5 +7,11 @@ const hasForbiddenStatus = (response: Response) => {
 };
 
 const hasServiceUnavailableStatus = (response: Response) => {
-  return response.status === 503;
+  return response.status === HttpStatusCode.FORBIDDEN_503;
 };
+
+// Тут почти все ок!
+// Инкапсуляция библиотеки открывает шанс скрыть низко уровневую реализацию
+// и в случае чего легко заменить библиотеку на другую. Иначе придется во всех местах использования
+// старой библиотеки менять как минимум импорты этой библиотеки на другую - это может оказаться дорого
+// и трудно
